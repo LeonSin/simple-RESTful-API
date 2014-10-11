b1983b8f92f5afe08506b6698bc1454c923a316c
tigerstudent <tigerstudent007@gmail.com>
2014-10-11 14:25:35 +0800

    version 0.99

--- README.md
+++ README.md
@@ -2,3 +2,39 @@ simple-RESTful-API
 ==================
 
 a simple RESTful API service to receive HTTP requests which can automatically parse request and store it appropriately, and send an email notification to the sender and a dedicated email account immediately.
+
+##程序依赖:
+本项目基于Python2.7开发,使用了`tornado`框架.
+##使用方法:
+1.将`config.py.example`改名成`config.py`,并按文件中的说明来完成必要配置.
+2.执行命令`python server.py`来运行.如果想将日志保存成文件形式,请加上运行参数-log_file_prefix,如`-log_file_prefix=/home/log.txt`.
+
+
+### Here is the field list for the request:
+
+| field          | Description                        | Type   |
+|----------------|------------------------------------|--------|
+| **email**      | Sender's email address             | string |
+| first_name     | Sender's first name                | string |
+| last_name      | Sender's last name                 | string |
+| contact_number | Sender's mobile phone number       | string |
+| title          | A title for the request            | string |
+| content        | a short description of the request | string |
+| link           | a web address                      | string |
+
+all fields above are required.
+Sample request
+--------------
+    $ curl -X POST -H "Accept: application/json" -d @data.json http://localhost:8000/rest
+
+    # data.json
+    {
+        "email": "tester@test.com",
+        "first_name": "Peter",
+        "last_name": "Pan",
+        "contact_number": "86-13227892789",
+        "title": "Request Title",
+        "content": "Request Content",
+        "link": "https://github.com"
+    }
+
--- server.py
+++ server.py
@@ -4,12 +4,13 @@ __author__ = 'tigerstudent'
 
 import json
 import re
+import datetime
+import logging
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
 import mailsending
 import config
-import datetime
 from tornado.options import define, options, parse_command_line
 
 define("port", default=8000, help="run on the given port", type=int)
@@ -17,7 +18,7 @@ define("port", default=8000, help="run on the given port", type=int)
 
 def check_email(email):
     """
-    check the format of email address
+    检查email参数是否符合一般邮箱格式
     :param email:
     :return:
     """
@@ -39,7 +40,7 @@ class Application(tornado.web.Application):
 class MainHandler(tornado.web.RequestHandler):
     def post(self):
         response = {}
-        request_body = json.loads(self.request.body)
+        request_body = json.loads(self.request.body)    # 解析接收到的json字符串
         email = request_body.get("email")
         first_name = request_body.get("first_name")
         last_name = request_body.get("last_name")
@@ -48,19 +49,33 @@ class MainHandler(tornado.web.RequestHandler):
         content = request_body.get("content")
         link = request_body.get("link")
 
+        #检验是否所有元素都非空
         if email is None or first_name is None or last_name is None or contact_number is None or title is None \
                 or content is None or link is None:
             response["error"] = "bad parameter"
             self.write(json.dumps(response))
             return
 
+        logging.info("receive request from %s" % email)     # 接收到有效请求
+
+        #检查email地址是否格式正确
         if check_email(email) is False:
+            logging.info("invalid email:%s" % email)
             response["error"] = "invalid email"
             self.write(json.dumps(response))
             return
 
-        send_to_sender(request_body)
-        send_to_dedicated_email(request_body)
+        #发送通知邮件给请求发送者
+        if send_to_sender(request_body):
+            logging.info("send notification succeed.To %s" % email)
+        else:
+            logging.info("send notification failed.To %s" % email)
+
+        #发送通知邮件给专用邮箱
+        if send_to_dedicated_email(request_body):
+            logging.info("send notification succeed.To %s" % email)
+        else:
+            logging.info("send notification failed.To %s" % email)
 
 
 def send_to_sender(request_body):
@@ -71,7 +86,7 @@ def send_to_sender(request_body):
     Thanks, Tech Team
     """ % (config.notification_email, request_body["email"], request_body["last_name"])
 
-    mailsending.send_mail(config.notification_email, request_body["email"], mail_content)
+    return mailsending.send_mail(config.notification_email, request_body["email"], mail_content)
 
 
 def send_to_dedicated_email(request_body):
@@ -81,7 +96,7 @@ def send_to_dedicated_email(request_body):
     """ % (config.notification_email, config.dedicated_email, request_body["email"], \
            request_body["last_name"], request_body["first_name"], datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-    mailsending.send_mail(config.notification_email, config.dedicated_email, mail_content)
+    return mailsending.send_mail(config.notification_email, config.dedicated_email, mail_content)
 
 
 def main():
